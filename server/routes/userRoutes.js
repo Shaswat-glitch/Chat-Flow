2503d321d3a44181125e40d8832c1cf3f48a1d44
shaswat-glitch <shaswat.kumar.mec22@itbhu.ac.in>
2024-07-07 02:08:45 +0530

    routes and controller for setAvatar has been setup

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const {register} = require("../Controllers/usersController")
 const {login} = require("../Controllers/usersController")
+const {setAvatar} = require("../Controllers/usersController")
 const router = require("express" ).Router();
 
 router.post("/register",register);
 router.post("/login",login)
+router.post("/setAvatar/:id",setAvatar)
 
 module.exports = router
