3e1c217a9ff9af42c513af945a77dca7f07f0b79
shaswat-glitch <shaswat.kumar.mec22@itbhu.ac.in>
2024-07-07 19:34:44 +0530

    push to github

--- server/index.js
+++ server/index.js
@@ -2,13 +2,16 @@ const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
 const userRoutes = require("./routes/userRoutes")
+const messageRoutes = require("./routes/messagesRoute")
 const app = express();
+const socket = require("socket.io");
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth",userRoutes)
+app.use("/api/messages",messageRoutes)
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology:true,
@@ -20,3 +23,25 @@ mongoose.connect(process.env.MONGO_URL,{
 const server = app.listen(process.env.PORT,()=>{
     console.log(`server started at ${process.env.PORT}`)
 })
+
+const io = socket(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      credentials: true,
+    },
+  });
+
+  global.onlineUsers = new Map();
+  io.on("connection", (socket) => {
+    global.chatSocket = socket;
+    socket.on("add-user", (userId) => {
+      onlineUsers.set(userId, socket.id);
+    });
+    socket.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if (sendUserSocket) {
+          socket.to(sendUserSocket).emit("msg-recieve", data.message);
+        }
+      });
+    });
+
